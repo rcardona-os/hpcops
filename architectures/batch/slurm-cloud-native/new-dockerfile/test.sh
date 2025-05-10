5103a8c2d5ff5f1f5ebce5476d099024189e5782
raf <rafael.cardona.it@gmail.com>
2025-05-11 01:26:16 +0200

    done

--- architectures/batch/slurm-cloud-native/new-dockerfile/test.sh
+++ architectures/batch/slurm-cloud-native/new-dockerfile/test.sh
@@ -2,17 +2,23 @@
 set -e
 
 # Set default MUNGEUSER if not already set
+echo "1- Set default MUNGEUSER if not already set"
 MUNGEUSER=${MUNGEUSER:-1001}
+echo "END 1-"
 
 # Check if the group exists before adding
+echo "2- Check if the group exists before adding"
 if ! getent group munge > /dev/null; then
     groupadd -g $MUNGEUSER munge
 fi
+echo "END 2-"
 
 # Check if the user exists before adding
+echo "3- Check if the user exists before adding"
 if ! getent passwd munge > /dev/null; then
     useradd -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge -s /sbin/nologin munge
 fi
+echo "END 3-"
 
 echo "Testing USER inside the container"
 echo "Running as user ID: $(id -u)"
@@ -20,9 +26,11 @@ echo "Running as group ID: $(id -g)"
 echo "Finished USER inside the container"
 
 # Create the munge key if it doesn't exist
+echo "4- Create the munge key if it doesn't exist"
 if [ ! -f /etc/munge/munge.key ]; then
     /usr/sbin/create-munge-key -r
 fi
+echo "END 4-"
 
 # Adjust ownership and permissions
 #chown $MUNGEUSER:$MUNGEUSER /etc/munge/munge.key
