2227d67d696e061d124d5163a5a0b4605e00f8ef
raf <rafael.cardona.it@gmail.com>
2025-05-20 22:03:24 +0200

    done

--- architectures/batch/slurm-cloud-native/new-dockerfile/entrypoint.sh
+++ architectures/batch/slurm-cloud-native/new-dockerfile/entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo ">> Unit Test 1 - Generating Key..."
-echo ">> Generating MUNGE key..."
-/usr/local/bin/create-munge-key -f -r
+#echo ">> Generating MUNGE key..."
+#/usr/local/bin/create-munge-key -f -r
 
-echo ">> Unit Test 2 - Creating Kubernetes Secret"
-echo ">> Creating Kubernetes Secret '$SECRET_NAME' in namespace '$SECRET_NAMESPACE'..."
-oc create secret generic "$SECRET_NAME" \
-  --from-file=munge.key=/mnt/munge/munge.key \
-  -n "$SECRET_NAMESPACE" --dry-run=client -o yaml | oc apply -f -
+#echo ">> Unit Test 2 - Creating Kubernetes Secret"
+#echo ">> Creating Kubernetes Secret '$SECRET_NAME' in namespace '$SECRET_NAMESPACE'..."
+#oc create secret generic "$SECRET_NAME" \
+#  --from-file=munge.key=/mnt/munge/munge.key \
+#  -n "$SECRET_NAMESPACE" --dry-run=client -o yaml | oc apply -f -
 
 echo ">> Secret created success."
